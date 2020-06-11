9c31fd90815b0811a7841a92ce5e951c07f2c295
Gaby Tal <45428645+gabytal@users.noreply.github.com>
2020-06-11 12:08:40 +0300

    Update and rename sortme.sh to decodeme.sh

--- decodeme.sh
+++ decodeme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#this script recive argument, please use sh sortme.sh $inputfile
+#this script recive argument, please use sh decodeme.sh $inputfile
 
 #replace spaces with new lines, sort -uniqe to remove duplicates, remove letters with sed, convert ASCII to sentence, assign to a variable
 value=$(tr ' ' '\n' < $1 | sort -u | sed 's/[A-Za-z]*//g'  |  awk '{ printf("%c",$0); }')
